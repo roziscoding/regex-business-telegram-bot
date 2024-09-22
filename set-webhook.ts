b7daa9cf4a0eaf1f7839e087f85618fcb8b87fc9
Roz <roz@rjmunhoz.me>
2024-09-22 11:52:14 -0300

    chore: update set-webhook script

--- set-webhook.ts
+++ set-webhook.ts
@@ -1,7 +1,10 @@
 import { Api } from "grammy/mod.ts";
 import { config } from "./env.ts";
 
+const url = Deno.args[0];
+console.log(`Setting webhook to ${url} with secret ${config.telegram.secret}`);
+
 const api = new Api(config.telegram.token);
 
-await api.setWebhook(Deno.args[0])
+await api.setWebhook(url, { secret_token: config.telegram.secret })
   .then(console.log);
